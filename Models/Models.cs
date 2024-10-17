4d9c4ef88272798c0f950cb1d69a4a612f10020d
Sabuhi <sabuhies-ab108@code.edu.az>
2024-10-17 13:11:55 +0400

    aded repo

---
+++ Models/Models.cs
@@ -0,0 +1,36 @@
+﻿namespace Models;
+
+public  class Manager
+{
+    private protected Employee GetPromotion(Employee employee)
+    {
+        employee.Salary += 100;
+        return employee;
+    }
+}
+
+public class Assistant : Manager
+{
+    public void GetFeedBack(Employee employee)
+    {
+        if (employee.IsSuccessful) GetPromotion(employee);
+    }
+}
+
+public class Employee
+{
+    private string _name ;
+    private bool _isSuccessful;
+    private float _salary;
+
+    public Employee(string name, bool isSuccessful, float salary)
+    {
+        Name = name;
+        IsSuccessful = isSuccessful;
+        Salary = salary;
+    }
+
+    public string Name { get { return _name; } set { _name = value; } }
+    public bool IsSuccessful { get { return _isSuccessful; } set { _isSuccessful = value; } }
+    public float Salary { get { return _salary; } set { _salary = value; } }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+
+namespace Test1;
+
+//  /\_/\
+// ( o.o )
+//   >^<
+
+internal class Program
+{
+    static void Main()
+    {
+        Assistant AngryAssistant = new Assistant();
+        
+        Employee TheBestEmployee = new Employee("Sabuhi", true, 1000f);
+        Employee TheWorstEmployee = new Employee("Ruslan", false, 200f);
+
+        Console.WriteLine(TheBestEmployee.Salary);
+        AngryAssistant.GetFeedBack(TheBestEmployee);
+        Console.WriteLine(TheBestEmployee.Salary);
+    }
+}
